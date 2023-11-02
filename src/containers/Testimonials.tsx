89d90391bf2f6149deab54144d8fef61c768cbeb
S-Christophe <christopheshao@gmail.com>
2023-11-02 09:20:37 +0100

    Add padding top to Testimonials section

--- src/containers/Testimonials.tsx
+++ src/containers/Testimonials.tsx
@@ -10,7 +10,7 @@ import datas from "../static/Testimonials.json";
  */
 export default function Testimonials() {
   return (
-    <section className="mx-auto flex flex-col gap-6 px-12 pb-[21.0625rem] md:flex-row md:justify-between lg:gap-10 lg:px-[3.71875rem] xl:max-w-7xl xl:pb-[22.5rem]">
+    <section className="mx-auto flex flex-col gap-6 px-12 pb-[21.0625rem] pt-[5.28125rem] md:flex-row md:justify-between lg:gap-10 lg:px-[3.71875rem] xl:max-w-7xl xl:pb-[22.5rem] xl:pt-[5.59375rem]">
       {datas.map((item, i) => (
         <Card key={i} {...item} />
       ))}
