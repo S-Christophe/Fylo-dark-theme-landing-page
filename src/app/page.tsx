bbcea2897a6b9de8b203e852127b2fb55c341e21
S-Christophe <christopheshao@gmail.com>
2023-11-04 13:11:49 +0100

    Create stay productive feature (mobile version)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // import containers
+import StayProductive from "@/containers/StayProductive";
 import Testimonials from "../containers/Testimonials";
 import AccessForm from "@/containers/AccessForm";
 
 export default function Home() {
   return (
     <main className="">
+      <StayProductive />
       <Testimonials />
       <AccessForm />
     </main>
---
+++ src/containers/StayProductive.tsx
@@ -0,0 +1,43 @@
+// import vendors
+import Image from "next/image";
+import Link from "next/link";
+
+export default function StayProductive() {
+  return (
+    <section className="flex flex-col items-center gap-12 px-7 py-20">
+      <Image
+        src="/assets/images/illustration-stay-productive.png"
+        alt="illustration stay productive"
+        width={615}
+        height={465}
+        className="h-[14.329375rem] w-[19rem]"
+      />
+      <div className="flex flex-col gap-4">
+        <h2 className="font-RalewayBold text-lg leading-6">
+          Stay productive, wherever you are
+        </h2>
+        <p className="font-OpenSans">
+          Never let location be an issue when accessing your files. Fylo has you
+          covered for all of your file storage needs.
+        </p>
+        <p className="font-OpenSans">
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </p>
+        <Link
+          href="#"
+          className="flex w-fit border-b border-cyan font-OpenSans text-xs text-cyan"
+        >
+          See how Fylo works
+          <Image
+            src="/assets/images/icon-arrow.svg"
+            alt="icon arrow"
+            width={16}
+            height={16}
+            className="mb-[0.375rem] ml-2"
+          />
+        </Link>
+      </div>
+    </section>
+  );
+}
