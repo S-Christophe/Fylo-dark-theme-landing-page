a61421f5a6396cedaa2551dc9091dfb4050166b0
S-Christophe <christopheshao@gmail.com>
2023-11-08 12:26:55 +0100

    Add curve to main div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import Testimonials from "@/containers/Testimonials";
 export default function Home() {
   return (
     <main className="">
-      <Features />
-      <StayProductive />
-      <Testimonials />
-      <AccessForm />
+      <div className='relative bg-dark-blue-main before:absolute before:-top-[8.8125rem] before:h-[8.8125rem] before:w-full before:bg-[url("../../public/assets/images/bg-curvy-mobile.svg")] before:bg-[length:100%_100%] before:bg-no-repeat lg:before:-top-[28.0625rem] lg:before:h-[28.0625rem] lg:before:bg-[url("../../public/assets/images/bg-curvy-desktop.svg")]'>
+        <Features />
+        <StayProductive />
+        <Testimonials />
+        <AccessForm />
+      </div>
     </main>
   );
 }
